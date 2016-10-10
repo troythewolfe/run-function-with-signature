12e828b7cfa52c22050aeb7b4c0b1ffeb70ecd5a
troythewolfe <troythewolfe@gmail.com>
2016-10-09 21:16:12 -0700

    tweaking class structure, example, docs

--- README.md
+++ README.md
@@ -1,19 +1,45 @@
-#run-function-with-signature
+#run-function-with-signature (SchemaFunction)
 
-`throw new Error(err)` if the input or output fails validation.
+## Description
 
-## Usage
+Name your function (ex. 'My Special Function With Signatures'), write your function body, optionally write a schema to validate the function's parameter object and/or the function's response object.
 
+If anything fails validation, `throw new Error(err)`.
+
+Set properties on construction using the 'params' parameter or set properties after construction using 'instance.property = value;' syntax.
+
+Export as simple, single property function.
+
+## API
 
 ```
 var SchemaFunction = require('run-function-with-signature');
 
+/**
+ * SchemaFunction
+ * Creates a function that will validate it's input and output against provided schemas.
+ * @type {class}
+ * @param {string} id - string indentifier, can include spaces.
+ * @param {object} params - an object to set the properties on construction
+ * @param {function} params.func - the function that this object runs
+ * @param {object} params.input - the input object for params.func
+ * @param {object} params.inputSchema - json schema describing the input object
+ * @param {object} params.outputSchema - json schema describing the output object
+ * @return {object} An instance of SchemaFunction
+**/
+
+## Usage
+
 /**/
-// USAGE
-let myFunc = new SchemaFunction('test function', { options: {
-    // available options
-    //responseRequired: false,
-  },
+let myFunc = new SchemaFunction('test function', { func: (input) => {
+
+    console.log(input);
+
+    return {
+      c: 'this is my response',
+      d: 3,
+    }
+  }
 });
 
 myFunc.inputSchema = {
@@ -22,14 +48,7 @@ myFunc.inputSchema = {
       b: { type: 'string' },
   },
   required: ['a', 'b'],
-}
-
-myFunc.func = (input) => {
-  return {
-    c: 'this is my response',
-    d: 3,
-  }
-}
+};
 
 myFunc.outputSchema = {
   properties: {
@@ -37,9 +56,9 @@ myFunc.outputSchema = {
       d: { type: 'integer' },
   },
   required: ['c', 'd'],
-}
+};
 
-//To export as standard function
+// Export as function
 function myRunFunction(input){
   return myFunc.run(input);
 }
@@ -49,6 +68,7 @@ const returnFromTheFunc = myRunFunction({
   b: 'string',
 });
 
+// returns { c: 'this is my response', d: 3 }
 console.log(returnFromTheFunc);
 /**/
 ```
--- run-function-with-signature.js
+++ run-function-with-signature.js
@@ -1,6 +1,85 @@
 var Validator = require('jsonschema').Validator;
 var v = new Validator();
 
+/**
+ * SchemaFunction
+ * Creates a function that will validate it's input and output against provided schemas.
+ * @type {class}
+ * @param {string} id - string indentifier, can include spaces.
+ * @param {object} params - an object to set the properties on construction
+ * @param {function} params.func - the function that this object runs
+ * @param {object} params.input - the input object for params.func
+ * @param {object} params.inputSchema - json schema describing the input object
+ * @param {object} params.outputSchema - json schema describing the output object
+ * @return {object} An instance of SchemaFunction
+**/
+class SchemaFunction {
+  constructor(id, params) {
+    this.id = id;
+    this.func = params.func;
+    this.input = params.input;
+    this.inputSchema = params.inputSchema;
+    this.outputSchema = params.outputSchema;
+  }
+
+  run(setInput){
+    this.input = setInput || this.input;
+
+    if (!this.id) new ThrowError('NO ID', `missing id`);
+
+    function validateInput(){
+      if (!this.inputSchema) return;
+
+      if (!this.inputSchema.properties)
+        new ThrowError(this.id, `input missing 'properties' property`);
+
+      //default additionalProperties to false
+      this.inputSchema.additionalProperties = this.inputSchema.additionalProperties === undefined
+        ? false : this.inputSchema.additionalProperties;
+
+      this.inputSchema.id = `${this.id}-input`;
+      this.inputSchema.type = 'object';
+
+      //validate input
+      var inputValidator = new Validator();
+      var inputValidation = inputValidator.validate(this.input, this.inputSchema);
+      if(inputValidation.errors.length)
+        new ThrowError(this.id, `input validation errors`, inputValidation.errors);
+    }
+
+    function validateOutput(){
+      if (!this.outputSchema) {
+        return this.func(this.input);
+      }
+
+      if (!this.outputSchema.properties)
+        new ThrowError(this.id, `output missing 'properties' property`);
+
+      this.outputSchema.additionalProperties = this.outputSchema.additionalProperties === undefined
+        ? false : this.outputSchema.additionalProperties;
+
+      this.outputSchema.id = `${this.id}-output`;
+      this.outputSchema.type = 'object';
+
+      //run function
+      var funcReturn = this.func(this.input);
+      if (!funcReturn)
+        new ThrowError(this.id, `function response undefined`);
+
+      //validate response
+      var ouputValidator = new Validator();
+      var ouputValidation = ouputValidator.validate(funcReturn, this.outputSchema);
+      if(ouputValidation.errors.length)
+        new ThrowError(this.id, `output validation errors`, ouputValidation.errors);
+
+      return funcReturn;
+    }
+
+    validateInput.apply(this);
+    return validateOutput.apply(this);
+  }
+}
+
 class ThrowError {
   constructor(id, message, error) {
     this.id = id;
@@ -10,106 +89,5 @@ class ThrowError {
     throw new Error(JSON.stringify(this));
   }
 }
-
-class SchemaFunction {
-  constructor(id, params) {
-    this.id = id;
-    this.func = params.func;
-    this.input = params.input;
-    this.inputSchema = params.inputSchema;
-    this.outputSchema = params.outputSchema;
-    this.options = params.options;
-  }
-
-  run(setInput){
-    function setIfDefined(option, alt){
-        return typeof option !== 'undefined' ? option : alt;
-    }
-
-    this.input = setInput || this.input;
-
-    let rr = this.options.responseRequired;
-
-    this.options = {
-        responseRequired: setIfDefined(rr, true)
-    };
-
-    //make sure they have an id and properties
-    if (!this.id) new ThrowError('NO ID', `missing id`);
-    if (!this.inputSchema.properties) new ThrowError(this.id, `input missing 'properties' property`);
-    if (!this.outputSchema.properties) new ThrowError(this.id, `output missing 'properties' property`);
-
-    //default additionalProperties to false
-    this.inputSchema.additionalProperties = this.inputSchema.additionalProperties === undefined
-      ? false : this.inputSchema.additionalProperties;
-    this.outputSchema.additionalProperties = this.outputSchema.additionalProperties === undefined
-      ? false : this.outputSchema.additionalProperties;
-
-    this.inputSchema.id = `${this.id}-input`;
-    this.outputSchema.id = `${this.id}-output`;
-    this.inputSchema.type = 'object';
-    this.outputSchema.type = 'object';
-
-    //validate input
-    var inputValidator = new Validator();
-    var inputValidation = inputValidator.validate(this.input, this.inputSchema);
-    if(inputValidation.errors.length) new ThrowError(this.id, `input validation errors`, inputValidation.errors);
-
-    //run function
-    var funcReturn = this.func(this.input);
-    if (this.options.responseRequired && !funcReturn) new ThrowError(this.id, `function response undefined`);
-
-    //validate response
-    var ouputValidator = new Validator();
-    var ouputValidation = ouputValidator.validate(funcReturn, this.outputSchema);
-    if(ouputValidation.errors.length) new ThrowError(this.id, `output validation errors`, ouputValidation.errors);
-
-    return funcReturn;
-  }
-}
-
-/**
-// USAGE
-let myFunc = new SchemaFunction('test function', { options: {
-    // available options
-    //responseRequired: false,
-  },
-});
-
-myFunc.inputSchema = {
-  properties: {
-      a: { type: 'integer' },
-      b: { type: 'string' },
-  },
-  required: ['a', 'b'],
-}
-
-myFunc.func = () => {
-  return {
-    c: 'this is my response',
-    d: 3,
-  }
-}
-
-myFunc.outputSchema = {
-  properties: {
-      c: { type: 'string' },
-      d: { type: 'integer' },
-  },
-  required: ['c', 'd'],
-}
-
-//To export as standard function
-function myRunFunction(input){
-  return myFunc.run(input);
-}
-
-const returnFromTheFunc = myRunFunction({
-  a: 3,
-  b: 'string',
-});
-
-console.log(returnFromTheFunc);
-/**/
 
 module.exports = SchemaFunction;
